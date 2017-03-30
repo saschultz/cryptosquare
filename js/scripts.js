993a3655b67ffa07e92870fde3d5816045915aa2
Spencer Ruiz and Sara Schultz <sara@>
2017-03-30 13:41:03 -0700

    add function to return a string with special characters and whitespace removed

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,20 @@ const lowerCase = function(sentence) {
   return sentence.toLowerCase();
 };
 
+const removeNonAlphanumeric = function(sentence) {
+  return sentence.replace(/[^a-z0-9]+/ig, "");
+};
+
+const encryptSentence = function(sentence) {
+  return removeNonAlphanumeric(lowerCase(sentence));
+};
+
 // front end
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     let userInput = $("#sentence").val();
-    $("#result").text(lowerCase(userInput));
+    $("#result").text(encryptSentence(userInput));
   });
 });
